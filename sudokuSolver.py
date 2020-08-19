c8384bd3c67c2e2f2959cee42e169bd8cd895acb
Marti Kandallu <marti.kandallu@gmail.com>
2020-08-19 10:15:33 -0700

    added check_box logic and example comments

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,7 +1,7 @@
 
 
 board = [
-    [7,8,0,4,0,0,1,2,0],
+    [7,8,0,4,0,0,1,2,0], #row 0
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
     [0,0,7,0,4,0,2,6,0],
@@ -12,6 +12,40 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+#check if board is valid
+#ex pos 4, 3
+def valid(board, num, pos):
+    # pos = (row, col)
+    row, col = pos
+    # row = pos[0]
+    # col = pos[1]
+    
+    #check row 0-8
+    for i in range(len(board[0])):
+        if board[row][i] == num and col != i:
+        return False
+    
+    #check column 0-8
+    for i in range(len(board)):
+        if board[i][col] == num and row != i:
+        return False
+
+    #check box
+    #ex. box_row = 3
+    #ex. box_col = 2
+    box_row = row // 3 #box_row = 1
+    box_col = col // 3 #bow_col = 0
+    
+    for i in range(box_row * 3, box_row * 3 + 3): #ex range(3, 6)
+        for j in range(box_col * 3, box_col * 3 + 3): #ex range(0, 3)
+            #loops through each element in the box looking for a duplicate number
+            if (board[i][j] == num and (i, j) != pos:
+                return False
+                
+    #if the pos makes it to the end, it is a valid position
+    return True
+    
+    
 def print_board(board):
     
     for i in range(len(board)):
@@ -34,7 +68,9 @@ def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 0:
-                return (i, j) #returns row, column 
+                return (i, j) #returns row, column
+
+                
     
 
            
